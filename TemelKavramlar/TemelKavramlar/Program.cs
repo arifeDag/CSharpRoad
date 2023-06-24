d0662b22eca3dae7cee9114fcc6c50b11244b2db
Arife Kara <arifedagkara@gmail.com>
2023-06-24 16:47:45 +0300

    gun 1 calısmları

---
+++ TemelKavramlar/TemelKavramlar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TemelKavramlar
+{
+    class Program
+    {
+
+        static string degisken1 = "Bu bir global Degisken";
+
+        static void Main(string[] args)
+        {
+
+            string degisken2 = "bu bir local degisken, method";
+
+            Console.WriteLine("merhaba Dünya");
+
+            Console.WriteLine(degisken1);
+
+            Console.WriteLine(degisken2);
+
+            for (int i = 0; i < 10; i++)
+            {
+                string degisken3 = $"Bu bir local degisken, dongü {i}";
+
+                Console.WriteLine(degisken3);
+                
+            }
+
+
+        }
+    }
+}
---
+++ TemelKavramlar/TemelVeriYapilari/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TemelVeriYapilari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad = "yavuz";
+            string soyısım = "kara";
+
+            int x = 17;
+            int y = 25;
+
+            int sonuc = x + y;
+
+            uint z = 0;
+            uint v = 4000;
+
+            short b = -200;
+            short c = 200;
+
+            ushort k =65000;
+            ushort t=0;
+
+            byte f =255;
+            Console.WriteLine("String");
+
+            Console.WriteLine(ad);
+            Console.WriteLine(soyısım);
+
+            Console.WriteLine("Integer");
+            Console.WriteLine(sonuc);
+
+            Console.WriteLine("uint");
+            Console.WriteLine(z);
+            Console.WriteLine(v);
+
+
+            Console.WriteLine("short");
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+            Console.WriteLine("ushort");
+            Console.WriteLine(k);
+            Console.WriteLine(t);
+
+            Console.WriteLine("byte");
+            Console.WriteLine(f);
+        }
+    }
+}
